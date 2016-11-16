07304de4253102aabbe2ea3532fecdf966d29ed5
crystalwang0124 <crystalwang0124@gmail.com>
2016-11-16 09:20:38 -0500

    TEST FOR COMMIT AND PUSH

---
+++ app/src/main/java/com/example/wangjing/gittest/MainActivityGit.java
@@ -0,0 +1,14 @@
+package com.example.wangjing.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityGit extends Activity {
+    //CHANGE TEST PUSH
+    //Try COMMIT AND PUSH
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_git);
+    }
+}
